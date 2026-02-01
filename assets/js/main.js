043dd656a8a00bf0d3ac69418558c2fa99c17e68
Mind Hustle <mindhustle.contact@gmail.com>
2026-02-01 13:33:18 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,12 +21,29 @@ setInterval(() => {
     slides[currentSlide].classList.add("active");
 }, 3000);
 // ===== HERO SLIDER END =====
+// ===== CONTACT SECTION  START =====
 const scriptURL = 'https://script.google.com/macros/s/AKfycbx_sVuKB5kTTh6V9HC6yH-WX5ddpTPwaWR12KqiuT1hovYf3d8jNvb9jLRZov9nlYwpLw/exec'
 const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
 
 form.addEventListener('submit', e => {
     e.preventDefault()
+
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => console.log('Success!', response))
-        .catch(error => console.error('Error!', error.message))
+        .then(response => {
+            msg.innerHTML = "✅ Form submitted successfully!"
+            msg.style.color = "green"
+
+            form.reset(); // inputs empty
+
+            setTimeout(() => {
+                msg.innerHTML = ""
+            }, 4000)
+        })
+        .catch(error => {
+            msg.innerHTML = "❌ Error! Try again"
+            msg.style.color = "red"
+        })
 })
+
+// ===== CONTACT SECTION END =====
